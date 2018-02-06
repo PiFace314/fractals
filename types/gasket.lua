d21e352e799adcb8fa44195aa37bfeb83b5cffe9
Henrique Santana <henriquessantana314@gmail.com>
2018-02-06 21:06:10 -0200

    New circles centers calculations
    Seems like the previous initial center values and previous calculations for the square root of the complex number were the cause of issue #1.

--- types/gasket.lua
+++ types/gasket.lua
@@ -6,6 +6,7 @@ local Complex = require 'complex'
 
 local limit = 4
 local size = 100
+local rotation = 0
 
 local cirSet = {}
 
@@ -43,8 +44,6 @@ local function tcFindCenters(r1,r2,r3,c1x,c1y,c2x,c2y,c3x,c3y)
 end
 
 
-
-
 function gasket.init()
     love.graphics.setColor(255,255,255)
     wd,ht = love.graphics.getDimensions()
@@ -55,26 +54,25 @@ end
 function gasket.reset()
     cirSet = {}
     collectgarbage()
-    z1 = Complex.new(-size,0)
-    z2 = Complex.new(size,0)
-    z3 = Complex.new(0,-math.sqrt(3)*size)
+    local r = 2*size/math.sqrt(3)
+    local rot = math.rad(rotation)
+    z1 = Complex.new((math.cos(rot-math.pi/2)*r)+wd/2,(math.sin(rot-math.pi/2)*r)+ht/2)
+    z2 = Complex.new((math.cos(math.pi*2/3+rot-math.pi/2)*r)+wd/2,(math.sin(math.pi*2/3+rot-math.pi/2)*r)+ht/2)
+    z3 = Complex.new((math.cos(math.pi*4/3+rot-math.pi/2)*r)+wd/2,(math.sin(math.pi*4/3+rot-math.pi/2)*r)+ht/2)
     r1,r2 = tcFindRadii(size,size,size)
     nc1,nc2 = tcFindCenters(size,size,size,z1.real,z1.imag,z2.real,z2.imag,z3.real,z3.imag)
-    print(r1,r2)
-    print(nc1,nc2)
 end
 
 function gasket.draw()
     love.graphics.setFont(fonts.text)
     love.graphics.setColor(255,255,255)
-    love.graphics.printf("Press 'Q' to decrease and 'W' to\n increase the number of steps.\n\nPress 'A' to decrease and 'S' to\n increase the size of the initial circle.",0,10,wd-10,'right')
+    love.graphics.printf("Press 'Q' to decrease and 'W' to\n increase the number of steps.\n\nPress 'A' to decrease and 'S' to\n increase the radii of the initial circles.\n\nScroll up or down to rotate.\nAngle: "..rotation.."°",0,10,wd-10,'right')
     
-    
-    love.graphics.circle('line',z1.real+wd/2,z1.imag+ht/2,size)
-    love.graphics.circle('line',z2.real+wd/2,z2.imag+ht/2,size)
-    love.graphics.circle('line',z3.real+wd/2,z3.imag+ht/2,size)
-    love.graphics.circle('line',nc1.real+wd/2,nc1.imag+ht/2,r1)
-    love.graphics.circle('line',nc2.real+wd/2,nc2.imag+ht/2,r2)
+    love.graphics.circle('line',z1.real,z1.imag,size)
+    love.graphics.circle('line',z2.real,z2.imag,size)
+    love.graphics.circle('line',z3.real,z3.imag,size)
+    love.graphics.circle('line',nc1.real,nc1.imag,r1)
+    love.graphics.circle('line',nc2.real,nc2.imag,r2)
     
 end
 
@@ -89,6 +87,18 @@ function gasket.keypressed(key)
         size = size + 10
     end
     gasket.reset()
+end
+
+function gasket.wheelmoved(x, y) 
+    if y > 0 then
+        rotation = rotation + 1
+        if rotation > 360 then rotation = -360 end
+        gasket.reset()
+    elseif y < 0 then
+        rotation = rotation - 1
+        if rotation < -360 then rotation = 360 end
+        gasket.reset()
+    end
 end
 
 function gasket.resize(w,h)
